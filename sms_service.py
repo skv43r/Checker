772b9c5a4d36fedb86041a9dbf75027485e4e69e
Sasha <skv43r@gmail.com>
2024-10-19 14:50:09 +0300

    refactor: moved SmsService class to a separate file

---
+++ sms_service.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+
+class SmsService:
+    def __init__(self, admin_id: int):
+        self.admin_id = admin_id
+
+    async def send_message(self, bot: Bot, message: str) -> None:
+        '''
+        Отправляет сообщение администратору с указанным текстом.
+        '''
+        await bot.send_message(self.admin_id, message)
