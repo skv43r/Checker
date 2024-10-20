1087f50c265a876c7ca55101da8a50d5faba9467
Alexandr <skv43r@gmail.com>
2024-10-20 14:01:48 +0300

    refactor(NumberChecker): replace requests with aiohttp

--- number_checker.py
+++ number_checker.py
@@ -1,6 +1,10 @@
-import requests
+import aiohttp
+import asyncio
+import json
 from aiogram import Bot
 from sms_service import SmsService
+from aiohttp import ClientTimeout
+
 
 class NumberChecker:
     def __init__(self,
@@ -19,41 +23,50 @@ class NumberChecker:
         Если номера найдены, функция отправляет сообщение администратору с
         количеством и ценой номеров.
         '''
-        try:
-            headers = {
-                'user-agent': (
-                    'Mozilla/5.0 (iPad; CPU OS 16_3 like Mac OS X) '
-                    'AppleWebKit/605.1.15 (KHTML, like Gecko) '
-                    'GSA/289.0.577695730 Mobile/15E148 Safari/604.1'
-                )
-            }
-            r = requests.get(self.url_sms_activate,
-                             headers=headers,
-                             timeout=10)
-            r.raise_for_status()
-            json_data = r.json()
-            for value in json_data.values():
-                if value.get("country") == 137 and value.get('price') <= 12:
-                    count_numbers = value.get("count")
-                    if count_numbers != 0:
-                        message = (
-                            f"Доступно {count_numbers} номеров по "
-                            f"цене {value.get('price')}"
-                        )
-                        await self.sms_service.send_message(bot, message)
-                        return True
-        except ConnectionError:
-            await self.sms_service.send_message(bot, "Нет соединения с API")
-        except TimeoutError:
-            await self.sms_service.send_message(bot, "Время ожидания истекло")
-        except requests.RequestException as e:
-            await self.sms_service.send_message(bot, f"Ошибка запроса: {e}")
-        except ValueError as e:
-            await self.sms_service.send_message(bot, f"Ошибка обработки "
-                                                     f"данных: {e}")
-        except Exception as e:
-            await self.sms_service.send_message(bot, f"Неожиданная ошибка: {e}")
-        
+        async with aiohttp.ClientSession() as session:
+            try:
+                response = await session.get(self.url_sms_activate)
+                response.raise_for_status()
+                try:
+                    json_data = await response.json()
+                except aiohttp.ContentTypeError:
+                    text_data = await response.text()
+                    try:
+                        json_data = json.loads(text_data)
+                    except ValueError:
+                        await self.sms_service.send_message(
+                            bot,
+                            f"Невозможно обработать ответ: {text_data}"
+                            )
+                        return False
+
+                for value in json_data.values():
+                    if value.get("country") == 137 and value.get('price') <= 9:
+                        count_numbers = value.get("count")
+                        if count_numbers != 0:
+                            message = (
+                                f"Доступно {count_numbers} номеров по "
+                                f"цене {value.get('price')}"
+                            )
+                            await self.sms_service.send_message(bot, message)
+                            return True
+
+            except aiohttp.ClientConnectionError:
+                await self.sms_service.send_message(bot,
+                                                    "Нет соединения с API")
+            except asyncio.TimeoutError:
+                await self.sms_service.send_message(bot,
+                                                    "Время ожидания истекло")
+            except aiohttp.ClientResponseError as e:
+                await self.sms_service.send_message(bot,
+                                                    f"Ошибка запроса: {e}")
+            except ValueError as e:
+                await self.sms_service.send_message(bot, f"Ошибка обработки "
+                                                         f"данных: {e}")
+            except Exception as e:
+                await self.sms_service.send_message(bot,
+                                                    f"Неожиданная ошибка: {e}")
+
         await self.sms_service.send_message(bot, "Нет доступных номеров")
         return False
 
@@ -62,16 +75,24 @@ class NumberChecker:
         Отправляет GET-запрос к API SMS Activate для получения баланса счета.
         Функция отправляет сообщение администратору с полученным балансом.
         '''
-        try:
-            r = requests.get(self.url_api_sms, timeout=10)
-            r.raise_for_status()
-            balance = r.text.split('ACCESS_BALANCE:')[-1]
-            await self.sms_service.send_message(bot, f"Баланс: {balance}")
-        except ConnectionError:
-            await self.sms_service.send_message(bot, "Нет соединения с API")
-        except TimeoutError:
-            await self.sms_service.send_message(bot, "Время ожидания истекло")
-        except requests.RequestException as e:
-            await self.sms_service.send_message(bot, f"Ошибка запроса: {e}")
-        except Exception as e:
-            await self.sms_service.send_message(bot, f"Неожиданная ошибка: {e}")
+        async with aiohttp.ClientSession() as session:
+            try:
+                response = await session.get(self.url_api_sms,
+                                             timeout=ClientTimeout(total=10))
+                response.raise_for_status()
+                text = await response.text()
+                balance = text.split('ACCESS_BALANCE:')[-1]
+                await self.sms_service.send_message(bot,
+                                                    f"Баланс: {balance}")
+            except aiohttp.ClientConnectionError:
+                await self.sms_service.send_message(bot,
+                                                    "Нет соединения с API")
+            except asyncio.TimeoutError:
+                await self.sms_service.send_message(bot,
+                                                    "Время ожидания истекло")
+            except aiohttp.ClientResponseError as e:
+                await self.sms_service.send_message(bot,
+                                                    f"Ошибка запроса: {e}")
+            except Exception as e:
+                await self.sms_service.send_message(bot,
+                                                    f"Неожиданная ошибка: {e}")
